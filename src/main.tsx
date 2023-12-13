462cd2472abf615811990e7db2f1ae2976849273
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-12-13 12:08:54 +0800

    updating the route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './Pages/RootLayout.tsx';
+import DashboardPage from './Pages/DashboardPage.tsx';
+import ReportsPage from './Pages/ReportsPage.tsx';
+import AccountPage from './Pages/Acounts.tsx';
+import TransactionsPage from './Pages/TransactionPage.tsx';
+import ProductPage from './Pages/Product.tsx';
 
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<RootLayout/>,
+    children:[
+      {
+        path:"/dashboard",
+        element:<DashboardPage/>
+      },
+      {
+        path:'/reports',
+        element:<ReportsPage/>
+      },
+      {
+        path:'/account',
+        element:<AccountPage/>
+      },
+      {
+        path:'/transaction',
+        element:<TransactionsPage/>
+      },
+      {
+        path:'/product',
+        element:<ProductPage/>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
