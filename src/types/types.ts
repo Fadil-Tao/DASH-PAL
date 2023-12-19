c288b31aec913dcb3352f2bcff6e0707e8843731
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-12-19 23:02:11 +0800

    add global types

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+export type chartProps = {
+    title:string
+    axis?:boolean
+}
+export type ChartDataValue = {
+    label: string;
+    income: number;
+    expense: number;
+}
+export type DummyCountryValue = {
+    label: string;
+    value: number;
+}
+
+export type CategoryValues = {
+    label: string;
+    value: number;
+}
