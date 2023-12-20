0c703f85b82764900a6cd2aacde0c98e34fc0432
Fadil-Tao <hadadfadilah2354@gmail.com>
2023-12-20 16:39:23 +0800

    justify some alignment

--- src/Pages/RootLayout.tsx
+++ src/Pages/RootLayout.tsx
@@ -1,21 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header';
 import SideDrawer from '../Components/SideDrawer';
-import { createContext, useState} from 'react';
-
+import { createContext, useState } from 'react';
 
 export const isOpenContext = createContext<boolean | any>(false);
 
 const RootLayout = () => {
-    const [isOpen,setIsopen] = useState<boolean | void>(false);
+    const [isOpen, setIsopen] = useState<boolean | void>(false);
     return (
-        <isOpenContext.Provider value={{isOpen, setIsopen}}  >
+        <isOpenContext.Provider value={{ isOpen, setIsopen }}>
             <div className='flex md:min-h-screen  md:max-w-screen'>
                 <SideDrawer />
-                <div className='w-full'>
+                <div className='w-full bg-black'>
                     <Header />
+
                     <div className=' bg-neutral-950 max-h-full min-h-screen  flex justify-start  items-start '>
-                        <div className='w-[98%] max-h-full min-h-screen rounded-md bg-zinc-900'>
+                        <div className='w-[98%] max-h-full min-h-screen rounded-xl my-4 bg-zinc-900'>
                             <Outlet />
                         </div>
                     </div>
